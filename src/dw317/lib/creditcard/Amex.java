7da77c2bbc827a97b634ee9699f4820730252ae1
1331680 <1331680@2F18-18.ad.dawsoncollege.qc.ca>
2016-09-22 10:44:12 -0400

    asdas

--- src/dw317/lib/creditcard/Amex.java
+++ src/dw317/lib/creditcard/Amex.java
@@ -2,24 +2,23 @@
  * 
  */
 package dw317.lib.creditcard;
-
 /**
  * @author Sebastian
  *
  */
-public final class MasterCard extends AbstractCreditCard{
+public final class Amex extends AbstractCreditCard{
 	private static final long serialVersionUID = 42031768871L;
 
 	/**
 	 * 
 	 */
-	public MasterCard(String number) {
+	public Amex(String number) {
 		// TODO Auto-generated constructor stub
-		super(CardType.MASTERCARD, validateNumber(number));
+		super(CardType.AMEX, validateNumber(number));
 	}
 	
 	private static String validateNumber(String number){
-		if (checkStartingDigits(number) || number.length()!=16){
+		if (checkStartingDigits(number) || number.length()!=15){
 			throw new IllegalArgumentException("INVALID NUMBER"); 
 		}
 		return number;
@@ -28,19 +27,10 @@ public final class MasterCard extends AbstractCreditCard{
 		boolean condition = true;
 		String firstTwoDigits = number.substring(0, 2);
 		switch (firstTwoDigits){
-		case "51":
+		case "34":
 			condition = false;
 			break;
-		case "52":
-			condition = false;
-			break;
-		case "53":
-			condition = false;
-			break;
-		case "54":
-			condition = false;
-			break;
-		case "55":
+		case "37":
 			condition = false;
 			break;
 		default:
